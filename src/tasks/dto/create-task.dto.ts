2752f8962377a9bb23bd8845ee24faa03ba7cc53
Isaque Maestri <isaquemaestri01@gmail.com>
2025-06-15 21:19:47 -0300

    feat: add DTOs and entity for task management including CreateTaskDto, GetTasksFilterDto, UpdateTaskDto, and Task entity

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,14 @@
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsNotEmpty({ message: 'Title is required' })
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsBoolean()
+  done: boolean;
+}
---
+++ src/tasks/dto/get-task.dto.ts
@@ -0,0 +1,7 @@
+import { IsOptional, IsBooleanString } from 'class-validator';
+
+export class GetTasksFilterDto {
+  @IsOptional()
+  @IsBooleanString({ message: 'done must be true or false' })
+  done?: string;
+}
