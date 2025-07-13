91ddd837238306a06b920d535f38c99c0f73fe52
Isaque Maestri <isaquemaestri01@gmail.com>
2025-07-13 19:16:40 -0300

    refactor: remove console.log statements from supabase client initialization

--- src/supabase/supabase.client.ts
+++ src/supabase/supabase.client.ts
@@ -6,5 +6,5 @@ const supabaseKey = process.env.SUPABASE_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing SUPABASE_URL or SUPABASE_KEY environment variable");
 }
-console.log(supabaseUrl, supabaseKey);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
